416dd92f437657e95df5d3c6a195db882ab1b2b7
egor5q <31898567+egor5q@users.noreply.github.com>
2019-07-28 02:16:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -67,7 +67,7 @@ def adddsfdgeh(m):
   
 @bot.message_handler(content_types=['photo'])
 def imgg(m):
-    bot.send_photo(441399484, m.photo[0].file_id, caption=str(m.text))
+    bot.send_photo(441399484, m.photo[0].file_id, caption=str(m.caption))
     p=pics.find_one({})
     if m.photo[0].file_id not in p['pics']:
         pics.update_one({},{'$push':{'pics':m.photo[0].file_id}})
