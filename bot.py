69f4052adb9ac420b4cb2775718b33eec5f0197c
egor5q <31898567+egor5q@users.noreply.github.com>
2020-06-29 17:53:45 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -933,7 +933,7 @@ def chlenomer(message):
   try:
     if m.chat.id not in wait_chats.find_one({})['chats']:
       print(bot.get_chat(m.chat.id).photo.big_file_id)
-      bot.send_photo(-1001324175427, bot.get_chat(m.chat.id).photo.big_file_id, caption = 'Найден новый чат: "'+m.chat.title+'" ('+str(m.chat.id)+') ('+str(m.chat.username)+')')
+      bot.send_photo(-1001324175427, bot.get_chat(m.chat.id).photo.big_file_id.split('_')[0], caption = 'Найден новый чат: "'+m.chat.title+'" ('+str(m.chat.id)+') ('+str(m.chat.username)+')')
       wait_chats.update_one({},{'$push':{'chats':m.chat.id}})
   except:
     bot.send_message(441399484, traceback.format_exc())
