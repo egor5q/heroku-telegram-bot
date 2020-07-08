c86196c48d38fab0d89c32c463abef81bd5d5c09
egor5q <31898567+egor5q@users.noreply.github.com>
2020-07-08 19:04:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -53,26 +53,34 @@ def aboutt(m, bot):
 
 
 def about(m, bot):
-    try:
-        threading.Thread(target = aboutt, args = [m, bot]).start()
-        return
-    except:
-        return
 
     a_u = about_user.find_one({'id':m.from_user.id})
     if a_u == None:
         about_user.insert_one(createabout(m))
         a_u = about_user.find_one({'id':m.from_user.id})
-    about_user.update_one({'id':m.from_user.id},{'$inc':{'msgcount':1}})
-    about_user.update_one({'id':m.from_user.id},{'$set':{'lastseen':time.time()}})
+        
+    msgcount = a_u['msgcount']
+    lastseen = a_u['lastseen']
+    names = a_u['names']
+    name = a_u['name']
+    username = a_u['username']
+    usernames = a_u['usernames']
+    groups = a_u['groups']
     
+    msgcount += 1
+    lastseen = time.time()
+    
+
     if m.from_user.first_name not in a_u['names']:
-        about_user.update_one({'id':m.from_user.id},{'$push':{'names':m.from_user.first_name}})
-        about_user.update_one({'id':m.from_user.id},{'$set':{'name':m.from_user.first_name}})
+        names.append(m.from_user.first_name)
+        name = m.from_user.first_name
     
     if m.from_user.username not in a_u['usernames']:
-        about_user.update_one({'id':m.from_user.id},{'$push':{'usernames':m.from_user.username}})
-        about_user.update_one({'id':m.from_user.id},{'$set':{'username':m.from_user.username}})
+        usernames.append(m.from_user.username)
+        username = m.from_user.username
         
     if str(m.chat.id) not in a_u['groups'] and m.chat.id < 0:
-        about_user.update_one({'id':m.from_user.id},{'$set':{'groups.'+str(m.chat.id):creategroup(m, bot)}})
+        groups.update({str(m.chat.id):creategroup(m, bot)})
+        
+    about_user.update_one({'id':m.from_user.id},{'$set':{'msgcount':msgcount, 'lastseen':lastseen, 'names':names, 'name':name, 'username':username, 'usernames':usernames, 'groups':groups}})
+    
