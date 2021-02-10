ed62f31708ce79802823ff25548ff88a785c2425
egor5q <31898567+egor5q@users.noreply.github.com>
2021-02-10 14:15:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -60,6 +60,37 @@ writed=[
 massive=['Хер','хер','Член','член','Хуй','хуй']
 elita=[]
 
+
+@bot.message_handler(commands=['testreklama'])
+def testrekkk(m):
+    if m.from_user.id != 441399484:
+        return
+    try:
+        bot.forward_message(chat_id = 441399484, from_chat_id = m.chat.id, message_id = m.reply_to_message.message_id)
+    except:
+        print(traceback.format_exc())
+        
+@bot.message_handler(commands=['reklama'])
+def testrekkkrrr(m):
+    if m.from_user.id != 441399484:
+        return
+    i = 0
+    for ids in idgroup.find({}):
+        try:
+            bot.forward_message(chat_id = ids['id'], from_chat_id = m.chat.id, message_id = m.reply_to_message.message_id)
+            i+=1
+            if i%1000 == 0:
+                try:
+                    bot.send_message(441399484, 'Сообщение получило '+str(i)+' чатов!')
+                except:
+                    pass
+        except:
+            pass
+    try:
+        bot.send_message(441399484, 'Сообщение получило '+str(i)+' чатов!')
+    except:
+        pass
+
 @bot.message_handler(commands=['combine'])
 def combine(m):
     config.about(m, bot)
